16797ce14781d6733ca64ca42d89ab5975ddebe9
Amresh Kumar <kamresh797@gmail.com>
2021-04-09 00:12:55 +0530

    Prime number program

---
+++ McaJavaLab/src/Amresh/com/Mayank/PrimeNumber.java
@@ -0,0 +1,31 @@
+
+
+package Amresh.com.Mayank;
+
+
+import java.util.Scanner;
+
+class PrimeNumber
+{
+	public static void main(String arg[])	
+	{
+	int i,count;
+         System.out.print("Enter n value : ");
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+    System.out.println("Prime numbers between 1 to "+n+" are ");
+	for(int j=2;j<=n;j++)
+	{
+	count=0;
+	for(i=1;i<=j;i++)
+	{
+	   if(j%i==0)
+	   {
+	        count++;        
+	   }
+	}
+	if(count==2)
+	       System.out.print(j+"  ");     
+	}
+	}
+}
